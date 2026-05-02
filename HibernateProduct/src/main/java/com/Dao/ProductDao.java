eec940e28e7a5b1185105c00e22c585c30fabd13
Ganesh <ganeshgarje716@gmail.com>
2026-05-02 18:34:55 +0530

    Create Table

--- HibernateProduct/src/main/java/com/Dao/ProductDao.java
+++ HibernateProduct/src/main/java/com/Dao/ProductDao.java
@@ -1,5 +1,16 @@
 package com.Dao;
 
+import org.hibernate.SessionFactory;
+
+import com.Config.HibernateConfig;
+
 public class ProductDao {
+	
+	
+	public static void main(String[] args) {
+		
+		SessionFactory sf = HibernateConfig.getSessionFactory();
+	}
+	
 
 }
