d37ab3a9e1aed7337ffdb365d63694171d240d90
Ganesh <ganeshgarje716@gmail.com>
2026-05-02 17:48:14 +0530

    In Product Class Create Variables, Create Constructor, Create Default Constructor, Create Getter Setter Methods and toString Method.

--- HibernateProduct/src/main/java/com/Entity/Product.java
+++ HibernateProduct/src/main/java/com/Entity/Product.java
@@ -1,5 +1,121 @@
 package com.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Column(unique=true)
+	private String name;
+	
+	private double price;
+	
+	private String category;
+	
+	private int qty;
+
+	
+	
+	
+	public Product(int id, String name, double price, String category, int qty) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.category = category;
+		this.qty = qty;
+	}
+	
+	
+	
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+
+	public int getQty() {
+		return qty;
+	}
+
+
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", category=" + category + ", qty=" + qty
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
